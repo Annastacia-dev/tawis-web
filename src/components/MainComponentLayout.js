571a0347b52816fd811dcf7fe123b43e9094d72e
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-03-26 15:33:29 +0300

    set up navbar component

---
+++ src/components/MainComponentLayout.js
@@ -0,0 +1,12 @@
+import NavBar from './NavBar';
+
+const MainComponentLayout = ({ children }) => {
+  return (
+    <div>
+      <NavBar />
+      {children}
+    </div>
+  )
+}
+
+export default MainComponentLayout
