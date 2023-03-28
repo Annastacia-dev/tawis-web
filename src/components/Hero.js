d40dc1f45b1354ce575013f59bea20b3e2b5881d
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-03-28 20:49:48 +0300

    build: create hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,26 +1,66 @@
+import { useState, useEffect } from 'react'
 import Image from "next/image"
+import images from '@/data/heroImages'
 import { BsArrowRight,BsArrowLeft } from 'react-icons/bs'
 
 const Hero = () => {
+
+console.log(images)
+  // change image after every 5 seconds 
+  const [current, setCurrent] = useState(0)
+  const length = images.length
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCurrent(current === length - 1 ? 0 : current + 1)
+    }, 5000)
+    return () => clearTimeout(timeout)
+  }, [current, length])
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1)
+  }
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1)
+  }
+
+  if (!Array.isArray(images) || images.length <= 0) {
+    return null
+  }
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 mt-10">
-      <div className="col-span-1 mt-10">
+    <div className="grid grid-cols-1 md:grid-cols-2 mt-12">
+      <div className="col-span-1">
         <h1 className="text-4xl font-bold font-playfair leading-[50px] tracking-wide">Welcome to Tawi&apos;s Bloom<span></span>    Kisumu&apos;s first luxury florist!</h1>
         <p className="text-sm mt-6 leading-8">
           We believe that flowers and gifts have the power to brighten someone&apos;s day and bring joy to any occasion. That&apos;s why we&apos;re dedicated to providing the highest quality products and services to our customers.
           Our team of talented florists and customer service experts is passionate about creating stunning floral designs that exceed your expectations. From romantic bouquets to elegant centerpieces, we have something for every occasion.
           At Tawi&apos;s Bloom, we specialize in all things flowers, indoor/outdoor plants, balloons, gifts, birthdays, weddings, and funerals. Our personalized service and customized floral designs are sure to make your event unforgettable.
         </p>
-        <button className="flex justify-center items-center gap-2 border border-black bg-transparent outline-none py-4 px-8 mt-10">
+        <button className="flex justify-center items-center gap-2 border border-black bg-transparent outline-none py-4 px-8 mt-10 hover:bg-black hover:text-white hover:transition duration-300">
           <span className="text-md font-light uppercase">Shop Now</span>
           <BsArrowRight className='text-sm ml-2' />
         </button>
       </div>
-      <div className="col-span-1 ml-28 relative">
-        <Image src="/10.jpeg" alt="hero" width={340} height={400} />
-        <div className="absolute bottom-[40%] right-16 flex flex-col gap-8 justify-center items-center">
-          <BsArrowRight className='h-6 w-6 ml-2' />
-          <BsArrowLeft className='h-6 w-6 ml-2' />
+      <div className="col-span-1 ml-0 sm:ml-28 sm:mt-0 mt-8 relative">
+        {
+          images.map((image, index) => {
+            return (
+              <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                {index === current && (
+                  <Image src={image.url} alt={image.alt} width={320} height={320}
+                  className="h-[200px] w-[200px] sm:h-[450px] sm:w-[300px] object-cover"
+                   />
+                )}
+              </div>
+            )
+          }
+          )
+        }
+        <div className="absolute bottom-[40%] right-1 sm:right-16 flex flex-col gap-8 justify-center items-center">
+          <BsArrowRight className='h-6 w-6 ml-2 cursor-pointer' onClick={nextSlide} />
+          <BsArrowLeft className='h-6 w-6 ml-2 cursor-pointer' onClick={prevSlide} />
         </div>
       </div>
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,6 +11,7 @@ const Navbar = () => {
   const toggleNav = () => setIsOpen(!isOpen);
 
   return (
+    <>
     <nav className="bg-white shadow-sm w-full">
       <div className="container mx-auto px-4">
         <div className="flex justify-between h-16">
@@ -21,20 +22,6 @@ const Navbar = () => {
                     <span className="font-bold tracking-wide">Tawi&apos;s Bloom</span>
                 </Link>
             </div>
-          <div className="-mr-2 flex items-center sm:hidden">
-            <button
-              type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-800 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-              onClick={toggleNav}
-            >
-              <span className="sr-only">Open main menu</span>
-              {isOpen ? (
-                <RiCloseLine className="h-6 w-6" aria-hidden="true" />
-              ) : (
-                <RiMenuFill className="h-6 w-6" aria-hidden="true" />
-              )}
-            </button>
-          </div>
           <div className="hidden md:flex items-center space-x-12">
             <Link href="/#about">About </Link>
             <Link href="/blog">Blog</Link>
@@ -48,16 +35,34 @@ const Navbar = () => {
         </div>
         </div>
       </div>
-      {isOpen && (
+    </nav>
+    <div className="sm:hidden block z-10">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md"
+              onClick={toggleNav}
+            >
+              <span className="sr-only">Open main menu</span>
+              {isOpen ? (
+                <RiCloseLine className="h-6 w-6" aria-hidden="true" />
+              ) : (
+                <RiMenuFill className="h-6 w-6" aria-hidden="true" />
+              )}
+            </button>
+          </div>
+    {isOpen && (
         <div className="sm:hidden w-full h-screen flex justify-center items-center duration-300 ease-in-out">
           <ul className="pt-2 pb-3 space-y-1">
-            <Link href="/" className="block px-3 py-2">Home</Link>
-            <Link href="/about" className="block px-3 py-2">About</Link>
-            <Link href="/contact" className="block px-3 py-2">Contact</Link>
+          <Link href="/cart" className='block px-3 py-2'><CgShoppingBag className='text-2xl ml-4' /></Link>
+            <Link href="/" className="text-xl block px-3 py-2">Home</Link>
+            <Link href="/about" className="text-xl block px-3 py-2">About</Link>
+            <Link href="/about" className="text-xl block px-3 py-2">Blog</Link>
+            <Link href="/contact" className="text-xl block px-3 py-2">Contact</Link>
+            <Link href="/shop" className="text-xl block px-3 py-2">Shop</Link>
           </ul>
         </div>
       )}
-    </nav>
+    </>  
   );
 };
 
